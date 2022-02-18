04c45767ff3a0654ddc493e4440772f525d03e2e
softwareCobbler <davidwrogers2@gmail.com>
2022-02-17 19:23:37 -0600

    add chrono

---
+++ readme.md
@@ -0,0 +1,3 @@
+### sudoku solver
+
+mostly an excuse to putz around with perf and c++/cmake
--- src/susolv.cpp
+++ src/susolv.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <chrono>
 #include <deque>
+#include <iostream>
 #include <optional>
+
 #include "susolv/board.h"
 
 Board loadBoard(const char* fname) {
@@ -75,14 +77,18 @@ std::optional<Board> solve(const Board& board) {
 
 int main(int argc)
 {
-    Board board = loadBoard("c:\\users\\anon\\dev\\rmme\\sudoku1.txt");
+    Board board = loadBoard("c:\\users\\anon\\dev\\susolv\\boards\\euler96-29.txt");
 
+    auto start = std::chrono::high_resolution_clock::now();
     std::optional<Board> maybeSolvedBoard = solve(board);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = end - start;
     if (maybeSolvedBoard) {
         std::cout << *maybeSolvedBoard << std::endl;
     }
     else {
         std::cout << "No solution." << std::endl;
     }
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed) << "ns // " << std::chrono::duration_cast<std::chrono::milliseconds>(elapsed) << "ms\n";
 }
 
